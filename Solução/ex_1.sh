b44a906a54cb1b571b95ed1b4da447d95efd7ee6
Luciano Pereira Sampaio <luciano.sampaio2004@gmail.com>
2023-03-31 16:33:05 -0300

    Corrigido método e resposta

--- Solução/ex_1.sh
+++ Solução/ex_1.sh
@@ -1,3 +1,1 @@
-cat ../Dados/machado/contos/*.txt ../Dados/machado/cronica/*.txt ../Dados/machado/critica/*.txt ../Dados/machado/miscelanea/*.txt ../Dados/machado/poesia/*.txt ../Dados/machado/romance/*.txt ../Dados/machado/teatro/*.txt ../Dados/machado/traducao/*.txt > ../Dados/machado/obra.txt
-
-echo "`wc -w ../Dados/machado/obra.txt`" > resposta_ex_1
+(cat ../Dados/machado/*/*.txt| wc -w) > resposta_ex_1
