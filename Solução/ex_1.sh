cd9c5c684c5aed632d2e65b4a98d116107ac54e0
Luciano Pereira Sampaio <luciano.sampaio2004@gmail.com>
2023-03-31 08:01:26 -0300

    ex_1 solucionado

---
+++ Solução/ex_1.sh
@@ -0,0 +1,3 @@
+cat ../Dados/machado/contos/*.txt ../Dados/machado/cronica/*.txt ../Dados/machado/critica/*.txt ../Dados/machado/miscelanea/*.txt ../Dados/machado/poesia/*.txt ../Dados/machado/romance/*.txt ../Dados/machado/teatro/*.txt ../Dados/machado/traducao/*.txt > ../Dados/machado/obra.txt
+
+echo "`wc -w ../Dados/machado/obra.txt`" > resposta_ex_1
